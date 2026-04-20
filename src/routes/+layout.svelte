2a87f758056ab03bb22170e26cef4b9ed8f8d859
OgwoGP <222779805+OgwoGPD@users.noreply.github.com>
2026-04-20 04:36:31 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -63,7 +63,7 @@
 	<link rel="icon" href="/favicon.png" />
 	<meta name="theme-color" content="#7C3AED" />
 
-		<link rel="apple-touch-icon" href="/icons/icon-192.png" />
+	<link rel="apple-touch-icon" href="/icons/icon-192.png" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 	<meta name="apple-mobile-web-app-title" content="Sabify" />
